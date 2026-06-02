d6fdda1f18ff3bf23ca24482966ce09f797eb596
Sriram Annamalai <sriramannamalai2002@gmail.com>
2026-06-03 00:54:05 +0530

    fixed the authorization in get_customer

--- frappe_utils/website_customization/api/customer.py
+++ frappe_utils/website_customization/api/customer.py
@@ -1,12 +1,14 @@
-import frappe 
+import frappe
 
 
 @frappe.whitelist()
 def get_customer(email):
-    query = """
-        SELECT parent from `tabPortal User` where user = %s
-    """
-    result = frappe.db.sql(query, (email,), as_dict=True)
-    if result:
-        return result[0]["parent"]
-    return None 
+	result = frappe.get_list(
+		"Portal User",
+		filters={"user": email},
+		fields=["parent"],
+		limit=1
+	)
+	if result:
+		return result[0].parent
+	return None
